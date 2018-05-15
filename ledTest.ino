be37878cf138a76bffe6d0fc4241f44b563a1c00
Flávio Luiz Puhl Junior <33373207+flaviopuhl@users.noreply.github.com>
2018-05-15 14:18:36 -0300

    Add files via upload

---
+++ ledTest.ino
@@ -0,0 +1,16 @@
+void ledTest(){
+   
+for (int thisPinBar = barCount - 1; thisPinBar >= 0; thisPinBar--) {
+  digitalWrite(barPins[thisPinBar], HIGH);
+        for (int thisPin = pinCount - 1; thisPin >= 0; thisPin--) {
+          // turn the pin on:
+          digitalWrite(ledPins[thisPin], HIGH);
+          delay(t1);
+          // turn the pin off:
+          digitalWrite(ledPins[thisPin], LOW);
+        }
+  digitalWrite(barPins[thisPinBar], LOW);
+}
+      
+}
+
